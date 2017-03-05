4341a379429e10dc5a6196511de8a62b1ff5cb13
EC2 Default User <ec2-user@ip-10-229-138-47.ec2.internal>
2017-03-05 23:02:38 +0000

    dl setup

---
+++ dlsetup.sh
@@ -0,0 +1,13 @@
+
+
+
+
+
+conda create -n dl python=3.5
+
+source activate dl
+conda install numpy pandas jupyter notebook matplotlib
+
+conda install scipy h5py
+pip install tensorflow
+pip install TFLearn
