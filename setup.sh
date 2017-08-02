4c4d7f07e1894097d4bb6927a1523379224c333b
Dale Cameron <dalecameron@outlook.com>
2017-08-02 19:33:47 +0000

    added nvm install to setup

--- setup.sh
+++ setup.sh
@@ -11,3 +11,5 @@ sudo yum install python-devel python3-devel
 
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --all
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
